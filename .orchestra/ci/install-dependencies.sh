4b74c7e273559ef9560b3e5cf6689da8901010db
Pietro Fezzardi <pietro@rev.ng>
2022-01-14 10:30:37 +0100

    Add jinja2 to install-dependencies.sh

--- .orchestra/ci/install-dependencies.sh
+++ .orchestra/ci/install-dependencies.sh
@@ -74,7 +74,7 @@ apt-get -qq install --no-install-recommends --yes \
 # * lit version should always match clang-release and llvm version
 # * pydot is incompatible with recent versions of pyparsing:
 #   https://github.com/pydot/pydot/issues/277
-pip3 -q install --user --upgrade setuptools wheel mako meson==0.56.2 pyelftools lit==12.0.0 pyparsing==2.4.7 pydot grandiso
+pip3 -q install --user --upgrade setuptools wheel mako meson==0.56.2 pyelftools lit==12.0.0 pyparsing==2.4.7 pydot grandiso jinja2
 
 if ! which git-lfs &> /dev/null; then
   curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | bash
