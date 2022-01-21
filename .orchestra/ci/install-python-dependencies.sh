660dbc0d95004f1297c65e0b1eb5223af1ccd255
Filippo Cremonese <filippocremonese@rev.ng>
2022-01-21 13:36:13 +0100

    Split python dependencies install script for CI
    Splits dependencies installation script (one installs dependencies from
    repositories, the other python dependencies from PYPI).

---
+++ .orchestra/ci/install-python-dependencies.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+PYTHON_DEPENDENCIES=(
+  setuptools
+  wheel
+  mako
+  "meson==0.56.2"
+  pyelftools
+  # lit version should always match clang-release and llvm version
+  "lit==12.0.0"
+  # pydot 3.0.2 introduced an incompatibility with pydot which is supposed to be resolved in a later version,
+  # but still causes problems to us (for instance, monotone framework headers generation fails), so we request v2.4.7.
+  # See https://github.com/pydot/pydot/issues/277
+  "pyparsing==2.4.7"
+  pydot
+  grandiso
+  Jinja2
+)
+
+pip3 -q install --user --upgrade "${PYTHON_DEPENDENCIES[@]}"
---
+++ .orchestra/ci/install-system-dependencies.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+set -e
+
+export DEBIAN_FRONTEND=noninteractive
+
+apt-get -qq update
+
+apt-get -qq install --no-install-recommends --yes \
+  aufs-tools \
+  autoconf \
+  automake \
+  bison \
+  build-essential \
+  ca-certificates \
+  cmake \
+  curl \
+  doxygen \
+  flex \
+  g++-multilib \
+  gawk \
+  git \
+  graphviz \
+  graphviz-dev \
+  jq \
+  libc-dev \
+  libexpat1-dev \
+  libglib2.0-dev \
+  liblzma-dev \
+  libncurses5-dev \
+  libreadline-dev \
+  libtool \
+  m4 \
+  ninja-build \
+  pkg-config \
+  python \
+  python3 \
+  python3-pip \
+  python3-dev \
+  python3-cffi \
+  python3-setuptools \
+  rsync \
+  sed \
+  ssh \
+  texinfo \
+  valgrind \
+  wget \
+  zlib1g-dev
+
+# Dependencies for Qt
+apt-get -qq install --no-install-recommends --yes \
+  gperf \
+  libcap-dev \
+  libfontconfig1-dev \
+  libfreetype6-dev \
+  libgl-dev \
+  libgl1-mesa-dev \
+  libgles2-mesa-dev \
+  libinput-dev \
+  libmount-dev \
+  libssl-dev \
+  libx11-dev \
+  libx11-xcb-dev \
+  libxcb-glx0-dev \
+  libxcb1-dev \
+  libxext-dev \
+  libxfixes-dev \
+  libxi-dev \
+  libxkbcommon-dev \
+  libxkbcommon-x11-dev \
+  libxrender-dev
+
+if ! which git-lfs &> /dev/null; then
+  curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | bash
+  apt-get -qq install --no-install-recommends --yes git-lfs
+fi
