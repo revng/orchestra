0ac61ad89efed8f1f16bda6947fa1fb870ba84e7
Alessandro Di Federico <ale@rev.ng>
2022-02-23 18:18:07 +0100

    Reorder python dependencies

--- .orchestra/ci/install-python-dependencies.sh
+++ .orchestra/ci/install-python-dependencies.sh
@@ -3,20 +3,20 @@
 set -e
 
 PYTHON_DEPENDENCIES=(
-  setuptools
-  wheel
-  mako
-  "meson==0.56.2"
-  pyelftools
+  "Jinja2"
+  "grandiso"
   # lit version should always match clang-release and llvm version
   "lit==12.0.0"
+  "mako"
+  "meson==0.56.2"
+  "pydot"
+  "pyelftools"
   # pydot 3.0.2 introduced an incompatibility with pydot which is supposed to be resolved in a later version,
   # but still causes problems to us (for instance, monotone framework headers generation fails), so we request v2.4.7.
   # See https://github.com/pydot/pydot/issues/277
   "pyparsing==2.4.7"
-  pydot
-  grandiso
-  Jinja2
+  "setuptools"
+  "wheel"
 )
 
 pip3 -q install --user --upgrade "${PYTHON_DEPENDENCIES[@]}"
