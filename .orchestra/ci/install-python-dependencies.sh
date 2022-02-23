45053ecd6f74117e584d52c752a6024d09a71feb
Alessandro Di Federico <ale@rev.ng>
2022-02-23 18:21:32 +0100

    Update Python dependencies
    Many dependencies are required for generatig Python wrappers for
    tuple_tree_generator data structures.

--- .orchestra/ci/install-python-dependencies.sh
+++ .orchestra/ci/install-python-dependencies.sh
@@ -3,18 +3,25 @@
 set -e
 
 PYTHON_DEPENDENCIES=(
+  "black"
+  "datamodel-code-generator"
   "Jinja2"
   "grandiso"
+  "graphlib-backport"
+  "jsonschema"
   # lit version should always match clang-release and llvm version
   "lit==12.0.0"
   "mako"
   "meson==0.56.2"
+  "networkx"
+  "pydantic"
   "pydot"
   "pyelftools"
   # pydot 3.0.2 introduced an incompatibility with pydot which is supposed to be resolved in a later version,
   # but still causes problems to us (for instance, monotone framework headers generation fails), so we request v2.4.7.
   # See https://github.com/pydot/pydot/issues/277
   "pyparsing==2.4.7"
+  "pyyaml"
   "setuptools"
   "wheel"
 )
