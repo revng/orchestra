78c279c7c6f1e70dbf1634c50b2380c8f84ee0f9
Giacomo Vercesi <giacomo.vercesi@rev.ng>
2022-04-12 13:31:17 +0200

    Add python packages for code checking in revng-check-conventions

--- .orchestra/ci/install-python-dependencies.sh
+++ .orchestra/ci/install-python-dependencies.sh
@@ -8,20 +8,27 @@ PYTHON_DEPENDENCIES=(
   "Jinja2"
   "grandiso"
   "graphlib-backport"
+  "isort"
   "jsonschema"
   # lit version should always match clang-release and llvm version
   "lit==12.0.0"
   "mako"
   "meson==0.56.2"
+  "mypy"
   "networkx"
   "pydot"
   "pyelftools"
+  "pylint"
   # pydot 3.0.2 introduced an incompatibility with pydot which is supposed to be resolved in a later version,
   # but still causes problems to us (for instance, monotone framework headers generation fails), so we request v2.4.7.
   # See https://github.com/pydot/pydot/issues/277
   "pyparsing==2.4.7"
   "pyyaml"
   "setuptools"
+  "types-backports"
+  "types-PyYAML"
+  "types-requests"
+  "types-urllib3"
   "wheel"
 )
 
