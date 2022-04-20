583e71804db0bf68f470493d6d50005c3b060206
Giacomo Vercesi <giacomo.vercesi@rev.ng>
2022-04-20 16:03:55 +0200

    Refactor install-python-dependencies.sh
    Move python dependencies to separate requirements.txt file

--- .orchestra/ci/install-python-dependencies.sh
+++ .orchestra/ci/install-python-dependencies.sh
@@ -1,44 +1,6 @@
 #!/bin/bash
+set -euo pipefail
 
-set -e
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-PYTHON_DEPENDENCIES=(
-  "black"
-  "cmakelang"
-  "flake8"
-  "flake8-breakpoint"
-  "flake8-builtins"
-  "flake8-comprehensions"
-  "flake8-eradicate"
-  "flake8-plugin-utils"
-  "flake8-polyfill"
-  "flake8-return"
-  "flake8-simplify"
-  "Jinja2"
-  "grandiso"
-  "graphlib-backport"
-  "isort"
-  "jsonschema"
-  # lit version should always match clang-release and llvm version
-  "lit==12.0.0"
-  "mako"
-  "meson==0.56.2"
-  "mypy"
-  "networkx"
-  "pep8-naming"
-  "pydot"
-  "pyelftools"
-  # pydot 3.0.2 introduced an incompatibility with pydot which is supposed to be resolved in a later version,
-  # but still causes problems to us (for instance, monotone framework headers generation fails), so we request v2.4.7.
-  # See https://github.com/pydot/pydot/issues/277
-  "pyparsing==2.4.7"
-  "pyyaml"
-  "setuptools"
-  "types-backports"
-  "types-PyYAML"
-  "types-requests"
-  "types-urllib3"
-  "wheel"
-)
-
-pip3 -q install --user --upgrade "${PYTHON_DEPENDENCIES[@]}"
+pip3 -q install --user --upgrade -r "${DIR}/requirements.txt"
