7fed7f352b6f961cbe4a59bea42e378c82219415
Alessandro Di Federico <ale@rev.ng>
2022-08-25 10:56:35 +0200

    Reorganize system dependencies
    Organize dependencies based on who requires them and purge some
    unnecessary dependencies.

--- .orchestra/ci/install-system-dependencies.sh
+++ .orchestra/ci/install-system-dependencies.sh
@@ -21,83 +21,89 @@ echo "deb [signed-by=/usr/share/keyrings/nodesource.gpg] https://deb.nodesource.
   > /etc/apt/sources.list.d/nodesource.list
 apt-get -qq update
 
-# Notes about installed packages:
-#
-# * winbind: `mspdb100.dll` tries to make some kind of connection that requires
-#   this package. Without it, the following command will fail:
-#
-#       orc install --test toolchain/win32-vc16/vc
+PACKAGES=()
 
-apt-get -qq install --no-install-recommends --yes \
-  aufs-tools \
-  autoconf \
-  automake \
-  bison \
-  build-essential \
-  cmake \
-  doxygen \
-  flex \
-  g++-multilib \
-  gawk \
-  git \
-  graphviz \
-  graphviz-dev \
-  jq \
-  libc-dev \
-  libexpat1-dev \
-  libglib2.0-dev \
-  liblzma-dev \
-  libncurses5-dev \
-  libreadline-dev \
-  libtool \
-  m4 \
-  msitools \
-  ninja-build \
-  nodejs \
-  p7zip-full \
-  pkg-config \
-  python \
-  python3 \
-  python3-cffi \
-  python3-dev \
-  python3-pip \
-  python3-setuptools \
-  rsync \
-  sed \
-  shellcheck \
-  ssh \
-  sudo \
-  texinfo \
-  unzip \
-  valgrind \
-  wget \
-  winbind \
-  winehq-stable \
-  xxd \
-  zlib1g-dev
+#
+# Base system tools
+#
+PACKAGES+=(python3)
+PACKAGES+=(python3-pip)
+PACKAGES+=(python3-setuptools)
+PACKAGES+=(gawk)
+PACKAGES+=(jq)
+PACKAGES+=(rsync)
+PACKAGES+=(sed)
+PACKAGES+=(ssh)
+PACKAGES+=(sudo)
+PACKAGES+=(unzip)
+PACKAGES+=(wget)
 
-# Dependencies for Qt
-apt-get -qq install --no-install-recommends --yes \
-  gperf \
-  libcap-dev \
-  libfontconfig1-dev \
-  libfreetype6-dev \
-  libgl-dev \
-  libgl1-mesa-dev \
-  libgles2-mesa-dev \
-  libinput-dev \
-  libmount-dev \
-  libssl-dev \
-  libx11-dev \
-  libx11-xcb-dev \
-  libxcb-glx0-dev \
-  libxcb1-dev \
-  libxext-dev \
-  libxfixes-dev \
-  libxi-dev \
-  libxkbcommon-dev \
-  libxkbcommon-x11-dev \
-  libxrender-dev
+#
+# Basic build tools
+#
+PACKAGES+=(autoconf)
+PACKAGES+=(automake)
+PACKAGES+=(bison)
+PACKAGES+=(build-essential)
+PACKAGES+=(cmake)
+PACKAGES+=(flex)
+PACKAGES+=(g++-multilib)
+PACKAGES+=(git)
+PACKAGES+=(libc-dev)
+PACKAGES+=(libtool)
+PACKAGES+=(m4)
+PACKAGES+=(ninja-build)
+PACKAGES+=(pkg-config)
+PACKAGES+=(texinfo)
+PACKAGES+=(zlib1g-dev)
+
+#
+# revng-orchestra build dependencies
+#
+# This is needed for python-Levenshtein
+PACKAGES+=(python3-dev)
+
+#
+# llvm-documentation dependencies
+#
+PACKAGES+=(doxygen)
+
+#
+# qemu dependencies
+#
+PACKAGES+=(libglib2.0-dev)
+PACKAGES+=(python2)
+
+#
+# revng dependencies
+#
+PACKAGES+=(python3-cffi)
+PACKAGES+=(doxygen)
+PACKAGES+=(shellcheck)
+PACKAGES+=(nodejs)
+PACKAGES+=(winehq-stable)
+PACKAGES+=(xxd)
+
+#
+# repackage-apple-sdk dependencies
+#
+PACKAGES+=(liblzma-dev)
+PACKAGES+=(p7zip-full)
+
+#
+# vs toolchains dependencies
+#
+PACKAGES+=(msitools)
+PACKAGES+=(p7zip-full)
+# winbind: `mspdb100.dll` tries to make some kind of connection that requires
+# this package. Without it, the following command will fail:
+#
+#     orc install --test toolchain/win32-vc16/vc
+#
+PACKAGES+=(winbind)
+PACKAGES+=(winehq-stable)
+
+apt-get -qq install --no-install-recommends --yes "${PACKAGES[@]}"
 
 if ! which git-lfs &> /dev/null; then
   curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | bash
