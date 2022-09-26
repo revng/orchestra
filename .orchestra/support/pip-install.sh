7cd928937c8e40820b931fd1eff20869c4650ec4
Alessandro Di Federico <ale@rev.ng>
2022-09-26 15:54:43 +0200

    Push all Python dependencies within orchestra

---
+++ .orchestra/support/pip-install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -euo pipefail
+
+python \
+    -m pip \
+    --disable-pip-version-check \
+    --no-python-version-warning \
+    install \
+    --no-index \
+    --compile \
+    --no-warn-script-location \
+    --find-links "${SOURCE_ARCHIVES}/python" \
+    "$@"
