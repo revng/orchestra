688464ce7d126e6a409d322bfb26956b1366c4cb
Giacomo Vercesi <giacomo.vercesi@rev.ng>
2025-06-04 10:02:07 +0200

    Add `ci/mass-testing-weeky`
    Add the CI yaml and auxiliary files that allow running mass-testing in
    the CI and publish it on the internet.

---
+++ .orchestra/ci/mass-testing-weekly/progress_poller.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+BASE_DIR="build/mass-testing/default"
+while true; do
+    sleep 1h
+
+    if [[ ( ! -d "$BASE_DIR/inputs" ) || ( ! -d "$BASE_DIR/build" ) ]]; then
+        continue
+    fi
+
+    TOTAL=$(find "$BASE_DIR/inputs" -type f | wc -l)
+    COUNT=$(find "$BASE_DIR/build" -name test-harness.json | wc -l)
+    PERCENT=$(python -c "print('{:03.2f}'.format($COUNT * 100 / $TOTAL))")
+
+    echo '------- MASS TESTING PROGRESS -------'
+    echo "Timestamp:  $(date --iso-8601=seconds)"
+    echo "Inputs processed: $(printf %10d "${COUNT}") ($PERCENT%)"
+    echo "Total:            $(printf %10d "${TOTAL}")"
+    echo '-------------------------------------'
+done
